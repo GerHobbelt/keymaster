61bb64bda9713edb3028d6851011d3943f2fca50
Ger Hobbelt <ger@hobbelt.com>
2013-02-05 17:36:48 +0100

    typo fixes in comments

--- keymaster.js
+++ keymaster.js
@@ -79,7 +79,7 @@
         for(k in _mods)
           if((!_mods[k] && index(handler.mods, +k) > -1) ||
             (_mods[k] && index(handler.mods, +k) == -1)) modifiersMatch = false;
-        // call the handler and stop the event if neccessary
+        // call the handler and stop the event if necessary
         if((handler.mods.length == 0 && !_mods[16] && !_mods[18] && !_mods[17] && !_mods[91]) || modifiersMatch){
           if(handler.method(event, handler)===false){
             if(event.preventDefault) event.preventDefault();
@@ -161,7 +161,7 @@
 
   function filter(event){
     var tagName = (event.target || event.srcElement).tagName;
-    // ignore keypressed in any elements that support keyboard data input
+    // ignore keypresses in any elements that support keyboard data input
     return !(tagName == 'INPUT' || tagName == 'SELECT' || tagName == 'TEXTAREA');
   }
 
